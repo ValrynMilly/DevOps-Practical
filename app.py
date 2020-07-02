557aeda4b790eb11e256764c5423b5fe1d0185ae
Emiljano <Emiljanokurbiba@gmail.com>
2020-07-03 00:30:56 +0200

    Logic for random generation services now complete
    Logic for random generation services now complete the next step would be to put them into their respective routes and have service one handle communication between all.

--- app.py
+++ app.py
@@ -14,6 +14,7 @@ import sys
 
 ####### FUNCTION TWO - NAME GENERATOR
 def namegen(input):
+    global username
     names = input.split(" ") ##This is going to be a get request
     orcnames = ['uk', 'guk', 'keg', 'rag', 'kha', 'rok', 'zok', 'huk', 'rik']
     elvnames = ['iros', 'ilir', 'tris', 'aren', 'ana', 'ina', 'enys', 'ona', 'dir']
@@ -22,7 +23,7 @@ def namegen(input):
         print("Please input your full and last name.")
         print("\n")
         print("Please try again")
-        namegen()
+        namegen('Emiljano Kurbiba')
         
     first_Name = names[0][:2]
     three_letters_surname = names[-1][:3]
@@ -35,6 +36,7 @@ namegen('Emiljano Kurbiba')
 
 ####### FUNCTION THREE - TITLE GENERATOR
 def titlegen():
+    global title
     articles = ['The', 'A']
     descriptive = ['Powerful', 'Dominant', 'Almighty', 'Omnipotent', 'Omniscient', 'Omnibenevolent', 'Ruling', 'Formidable', 'Divine' ,'Hero']
     objective = ['One', 'Demi-God', 'Giant', 'Berzerker', 'Hunter', 'Wizard', 'Born', 'God']
@@ -44,3 +46,18 @@ def titlegen():
     print(title.upper())
 
 titlegen()
+
+print('\n')
+
+####### FUNCTION FOUR - SHORT STORY GENERATOR INCLUDING TWO AND THREE
+
+def storygen():
+    where = ['In a bar','On a field', 'Through the clouds', 'Under a mountain', 'Underwater', 'In a town']
+    action = ['killed', 'annihilated', 'destroyed', 'Poisoned', 'Petrified', 'Saved', 'Rescued', 'Healed', 'Silenced']
+    instrument = ['a pinkie', 'a pencil', 'a hammer', 'a bow', 'lightning', 'fire', 'a hunt']
+
+    shortstory = (random.choice(where) + " " + username + " " + title +" Once " + random.choice(action)+ " " + str(random.randint(1, 3000)) + " MEN WITH " + random.choice(instrument)) 
+## This function is utalising globally accessible variables from other function/services which will inevitably be handled through requests
+    print(shortstory.upper())
+
+storygen()
