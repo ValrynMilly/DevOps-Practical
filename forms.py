541d471d2a7f9348e88d1c597eb0ade7552f3ea4
Emiljano <Emiljanokurbiba@gmail.com>
2020-07-07 00:34:29 +0200

    Service three removed forms & templates
    reeason for removing forms & templates is that service one will handle it

--- forms.py
+++
@@ -1,7 +0,0 @@
-from flask_wtf import FlaskForm
-from wtforms import StringField, TextField, SubmitField
-from wtforms.validators import DataRequired
-
-class titlegen(FlaskForm):
-    #Title generation
-    submit = SubmitField('Generate')
--- service-three.py
+++ service-three.py
@@ -1,5 +1,4 @@
 from flask import Flask, url_for, render_template, redirect, request
-from forms import titlegen
 
 import string
 import random
@@ -7,21 +6,13 @@ import random
 app = Flask(__name__)
 app.config['SECRET_KEY']='Thisisanothersupersecret'
 
-@app.route('/')
-def titlegenform():
-    form = titlegen(request.form)
-    return render_template('titlegen.html', title='titlegen', form=form)
-
-@app.route('/', methods=['POST'])
+@app.route('/', methods=['GET'])
 def titlegen_post():
-    form = titlegen()
     articles = ['The', 'A']
     descriptive = ['Powerful', 'Dominant', 'Almighty', 'Omnipotent', 'Omniscient', 'Omnibenevolent', 'Ruling', 'Formidable', 'Divine' ,'Hero']
     objective = ['One', 'Demi-God', 'Giant', 'Berzerker', 'Hunter', 'Wizard', 'Born', 'God'] 
-    if form.is_submitted():
-        title_generated = (random.choice(articles) + " " + random.choice(descriptive) + " " + random.choice(objective)) 
-        return title_generated.upper()
-
+    title_generated = (random.choice(articles) + " " + random.choice(descriptive) + " " + random.choice(objective)) 
+    return title_generated.upper()
 
 if __name__ == '__main__':
- app.run(debug=True, host='127.0.0.2')
+ app.run(debug=True, host='127.0.0.3')
