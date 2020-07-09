186750bed9d167993918450889a88be4d1a53289
Emiljano <Emiljanokurbiba@gmail.com>
2020-07-09 15:44:11 +0200

    Testing comms between all

--- service-four/service-four.py
+++ service-four/service-four.py
@@ -3,7 +3,7 @@ import string
 import sys
 import requests
 
-from flask import Flask, url_for, render_template, redirect, request
+from flask import Flask, url_for, render_template, redirect, request, Response
 
 app = Flask(__name__)
 app.config['SECRET_KEY']='Thisisanothersuperdupersecret'
@@ -13,9 +13,9 @@ def titlegen_post():
     where = ['In a bar','On a field', 'Through the clouds', 'Under a mountain', 'Underwater', 'In a town']
     action = ['killed', 'annihilated', 'destroyed', 'Poisoned', 'Petrified', 'Saved', 'Rescued', 'Healed', 'Silenced']
     instrument = ['a pinkie', 'a pencil', 'a hammer', 'a bow', 'lightning', 'fire', 'a hunt'] 
-    shortstory = (random.choice(where) + " " + "USERNAME" + " " + "title" +" Once " + random.choice(action)+ " " + str(random.randint(1, 3000)) + " MEN WITH " + random.choice(instrument)) 
+    shortstory = (random.choice(where) + " " + name + " " + title +" Once " + random.choice(action)+ " " + str(random.randint(1, 3000)) + " MEN WITH " + random.choice(instrument)) 
     #so USERNAME & TITLE will be post request sent from service one which will be read then generated
-    return shortstory.upper()
+    return Reponse(shortstory, mimetype='text/plain')
 
 if __name__ == '__main__':
  app.run(host='0.0.0.0', port=5003, debug=True)
--- service-three/service-three.py
+++ service-three/service-three.py
@@ -1,4 +1,4 @@
-from flask import Flask, url_for, render_template, redirect, request
+from flask import Flask, url_for, render_template, redirect, reques, Response
 import requests
 import string
 import random
@@ -6,13 +6,13 @@ import random
 app = Flask(__name__)
 app.config['SECRET_KEY']='Thisisanothersupersecret'
 
-@app.route('/', methods=['GET'])
+@app.route('/', methods = ['GET', 'POST'])
 def titlegen_post():
     articles = ['The', 'A']
     descriptive = ['Powerful', 'Dominant', 'Almighty', 'Omnipotent', 'Omniscient', 'Omnibenevolent', 'Ruling', 'Formidable', 'Divine' ,'Hero']
     objective = ['One', 'Demi-God', 'Giant', 'Berzerker', 'Hunter', 'Wizard', 'Born', 'God'] 
     title_generated = (random.choice(articles) + " " + random.choice(descriptive) + " " + random.choice(objective)) 
-    return title_generated.upper()
+    return Response(title_generated, mimetype='type/plain')
 
 if __name__ == '__main__':
  app.run(host='0.0.0.0', port=5002, debug=True)
