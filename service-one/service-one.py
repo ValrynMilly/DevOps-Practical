ff940ac688f9037464b447128086b8d5487b06ea
Emiljano <Emiljanokurbiba@gmail.com>
2020-07-09 17:01:25 +0200

    filestructure redo

--- service-one/service-one.py
+++ service-one/service-one.py
@@ -1,29 +1,4 @@
-from flask import Flask, url_for, render_template, redirect, request
-from forms import namegen
-import requests
-import string
-import random
-
-app = Flask(__name__)
-servicetwo = 'http://service-two:5001'
-servicethree = 'http://service-three:5002'
-servicefour = 'http://service-four:5003'
-
-app.config['SECRET_KEY']='Thisisanothersupersupersupersecret'
-
-@app.route('/')
-def namegenform():
-    form = namegen(request.form)
-    return render_template('namegen.html', title='namegen', form=form)
-
-@app.route('/', methods = ['POST'])
-def namegenform_post():
-    form = namegen()
-    if form.validate_on_submit():
-        story = requests.get(servicefour)
-        result = requests.post(servicefour, story.text)
-        return result.text
-
+from application import app
 
 if __name__ == '__main__':
  app.run(host='0.0.0.0', port=5000, debug=True)
