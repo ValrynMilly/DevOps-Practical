a906abe37ef4b9a02abb0f89e7d2be3bb935c527
Emiljano <Emiljanokurbiba@gmail.com>
2020-07-10 11:21:36 +0200

    variables redo

--- service-one/application/__init__.py
+++ service-one/application/__init__.py
@@ -5,5 +5,5 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 db = SQLAlchemy(app)
 app.config['SECRET_KEY'] = getenv("SECRET_KEY")
-app.config['SQLALCHEMY_DATABASE_URI'] = str(os.getenv('DATABASE_URI'))
+app.config['SQLALCHEMY_DATABASE_URI'] = str(os.getenv('SQLALCHEMY_DATABASE_URI'))
 from application import routes
