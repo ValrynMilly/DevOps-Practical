9672e3eaf393d5f94cbca48acdb4ff7e88236f67
Emiljano <Emiljanokurbiba@gmail.com>
2020-07-11 01:16:48 +0200

    update stack

--- jenkins/scripts/stacksonstacks.sh
+++ jenkins/scripts/stacksonstacks.sh
@@ -5,6 +5,6 @@ git clone https://github.com/ValrynMilly/DevOps-Practical.git -b Jenkins
 cd DevOps-Practical
 
 sudo docker volume create fantasygens
-
+docker stack rm namegenstack
 sudo env password=${password} DATABASE_URI=${DATABASE_URI} docker stack deploy --compose-file docker-compose.yaml namegenstack
 EOF
