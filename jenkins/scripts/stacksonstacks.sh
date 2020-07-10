fc3948e9aa74f02198f172f65ac2b7381a282736
Emiljano <Emiljanokurbiba@gmail.com>
2020-07-11 00:36:52 +0200

    updated compose and added first image for jenkins

---
+++ jenkins/scripts/stacksonstacks.sh
@@ -0,0 +1,7 @@
+ssh -t jenkins@jenkins<< EOF
+git clone https://github.com/ValrynMilly/DevOps-Practical.git
+cd /home/jenkins/DevOps-Practical
+git checkout Jenkins
+sudo docker volume create fantasygens
+sudo env password=${password} DATABASE_URI=${DATABASE_URI} docker stack deploy --compose-file docker-compose.yaml namegenstack
+EOF
