1321d40479defca18fe6350251b0182114fa9c9b
Emiljano <Emiljanokurbiba@gmail.com>
2020-07-11 15:16:06 +0200

    verifying ansible is installed on install script

--- jenkins/scripts/ansibleinstallverify.sh
+++ jenkins/scripts/ansibleinstallverify.sh
@@ -10,6 +10,8 @@ sudo apt install python3 -y
 sudo apt install python3-pip -y
 # Simply installing ansible on current user
 pip3 install --user ansible
+# Verifying Ansible is installed will show on logs
+ansible --version
 #locate to root
 cd /home/jenkins/
 #Running inventory & playbook
