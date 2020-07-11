11f7bf14cb409409286aae27e20b561e7c34fce2
Emiljano <Emiljanokurbiba@gmail.com>
2020-07-11 13:39:42 +0200

    install ansible script for pipeline

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,9 +1,14 @@
 pipeline{
   agent any
   stages{
-    stage('Docker Stack'){
+    stage('Stack Set-up'){
       steps{
         sh "bash jenkins/scripts/stacksonstacks.sh"   
+      }
+    }
+    stage('Ansible Install & Verify'){
+      steps{
+        sh "bash Jenkins/scripts/ansibleinstallverify.sh"
       }
     }
   }
---
+++ jenkins/scripts/ansibleinstallverify.sh
@@ -0,0 +1,13 @@
+# Making sure ~/.local/bin Exists
+mkdir -p ~/.local/bin
+echo 'PATH=$PATH:~/.local/bin' >> .bashrc
+source .bashrc
+#installing prerequisites to install ansible
+#python 3 & pip is needed to to install ansible
+sudo apt update -y
+sudo apt install python3 -y
+sudo apt install python3-pip -y
+#Simply installing ansible on current user
+pip3 install --user ansible
+#Running local inventory & playbook
+ansible-playbook -i ansible/inventory ansible/playbook.yaml
