04a16af090f4c6921e4f21988a3353c0cb3e1b42
Emiljano <Emiljanokurbiba@gmail.com>
2020-07-12 01:15:49 +0200

    requirements updated

--- jenkins/scripts/testing.sh
+++ jenkins/scripts/testing.sh
@@ -2,4 +2,5 @@ sudo apt update -y
 sudo apt install python3-pip python3-venv -y
 
 pip3 install flask flask-testing pytest pytest-cov requests
+pip3 install requests_mock
 python3 -m pytest ./service-one/tests/test_unit.py
