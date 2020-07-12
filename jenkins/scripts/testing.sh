896c088e6bc2a93db272dffa329cf082b8917799
Emiljano <Emiljanokurbiba@gmail.com>
2020-07-12 19:41:18 +0200

    update script

--- jenkins/scripts/testing.sh
+++ jenkins/scripts/testing.sh
@@ -3,5 +3,5 @@ cd DevOps-Practical
 git pull
 echo $TEST_DB_URI
 cd service-one
-sudo env TEST_DB_URI=${TEST_DB_URI} python3 -m pytest ./service-one/tests/test_unit.py
+sudo env TEST_DB_URI=${TEST_DB_URI} python3 -m pytest --cov service-one
 EOF
