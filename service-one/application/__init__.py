cfefcac1f18541749ab19b9a5e98b87af4a24232
Emiljano <Emiljanokurbiba@gmail.com>
2020-07-12 14:43:07 +0200

    update init.py for test database

--- service-one/application/__init__.py
+++ service-one/application/__init__.py
@@ -6,4 +6,5 @@ app = Flask(__name__)
 db = SQLAlchemy(app)
 app.config['SECRET_KEY'] = getenv("SECRET_KEY")
 app.config['SQLALCHEMY_DATABASE_URI'] = str(os.getenv('SQLALCHEMY_DATABASE_URI'))
+app.config['SQLALCHEMY_DATABASE_URI'] = str(os.getenv('TEST_DB_URI'))
 from application import routes
