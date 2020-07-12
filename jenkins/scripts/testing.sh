cc451d07855bcbd1d9cca482b9974d8ebe95f0dc
Emiljano <Emiljanokurbiba@gmail.com>
2020-07-12 20:06:12 +0200

    including fourth test in script

--- jenkins/scripts/testing.sh
+++ jenkins/scripts/testing.sh
@@ -6,4 +6,5 @@ cd service-one
 sudo env TEST_DB_URI=${TEST_DB_URI} python3 -m pytest --cov service-one
 sudo env TEST_DB_URI=${TEST_DB_URI} python3 -m pytest --cov service-two/tests
 sudo env TEST_DB_URI=${TEST_DB_URI} python3 -m pytest --cov service-three/tests
+sudo env TEST_DB_URI=${TEST_DB_URI} python3 -m pytest --cov service-four/tests
 EOF
