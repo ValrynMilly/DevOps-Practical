0d34a6b60c8fbba513ac60b57224b3cadac8d14c
Emiljano <Emiljanokurbiba@gmail.com>
2020-07-12 19:38:41 +0200

    update testing script

--- jenkins/scripts/testing.sh
+++ jenkins/scripts/testing.sh
@@ -1,13 +1,7 @@
 ssh jenkins@projectmanager << EOF
-sudo rm -rf DevOps-Practical
-git clone https://github.com/ValrynMilly/DevOps-Practical.git -b Jenkins
+cd DevOps-Practical
+git pull
 echo $TEST_DB_URI
 cd service-one
-sudo env TEST_DB_URI=${TEST_DB_URI} python3 -m pytest --cov application --cov-report term-missing
-cd ..
-cd service-two
-sudo env TEST_DB_URI=${TEST_DB_URI} python3 -m pytest --cov tests --cov-report term-missing
-cd ..
-cd service-three
-sudo env TEST_DB_URI=${TEST_DB_URI} python3 -m pytest --cov tests --cov-report term-missing
+sudo env TEST_DB_URI=${TEST_DB_URI} python3 -m pytest ./service-one/tests/test_unit.py
 EOF
