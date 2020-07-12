020ca9540cf1cd791e172c32638d42ca23b01894
Emiljano <Emiljanokurbiba@gmail.com>
2020-07-12 19:45:50 +0200

    adding reporting in testing

--- jenkins/scripts/testing.sh
+++ jenkins/scripts/testing.sh
@@ -3,7 +3,7 @@ cd DevOps-Practical
 git pull
 echo $TEST_DB_URI
 cd service-one
-sudo env TEST_DB_URI=${TEST_DB_URI} python3 -m pytest --cov service-one
-sudo env TEST_DB_URI=${TEST_DB_URI} python3 -m pytest --cov service-two/tests
-sudo env TEST_DB_URI=${TEST_DB_URI} python3 -m pytest --cov service-three/tests
+sudo env TEST_DB_URI=${TEST_DB_URI} python3 -m pytest --cov service-one --cov-report
+sudo env TEST_DB_URI=${TEST_DB_URI} python3 -m pytest --cov service-two/tests --cov-report
+sudo env TEST_DB_URI=${TEST_DB_URI} python3 -m pytest --cov service-three/tests --cov-report
 EOF
