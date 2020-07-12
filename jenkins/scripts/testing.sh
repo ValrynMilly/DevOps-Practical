4003b0a2d7933f074e4151c5880406b18972483c
Emiljano <Emiljanokurbiba@gmail.com>
2020-07-12 16:34:49 +0200

    Updating varibale should work now

--- jenkins/scripts/testing.sh
+++ jenkins/scripts/testing.sh
@@ -1,7 +1,7 @@
 ssh jenkins@projectmanager << EOF
 cd DevOps-Practical
 git pull
-export TEST_DB_URI='mysql+pymysql://root:Le0535892@35.242.172.253/testfantasygendb'
+sudo env TEST_DB_URI=${TEST_DB_URI}
 echo $TEST_DB_URI
 python3 -m pytest --cov service-one/application --cov-report term-missing
 EOF
