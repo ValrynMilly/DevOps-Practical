b01a0c443d79b2338bb116e1b8b7c7c87417556d
Emiljano <Emiljanokurbiba@gmail.com>
2020-07-12 16:22:06 +0200

    update script

--- jenkins/scripts/testing.sh
+++ jenkins/scripts/testing.sh
@@ -4,7 +4,5 @@ git clone https://github.com/ValrynMilly/DevOps-Practical.git -b Jenkins
 
 cd DevOps-Practical
 
-sudo env TEST_DB_URI=${TEST_DB_URI}
-sudo env SQLALCHEMY_DATABASE_URI=${TEST_DB_URI}
 python3 -m pytest --cov application --cov-report term-missing
 EOF
