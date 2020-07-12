7cfce89f19026fb9035cfa926a4c4fac449e1102
Emiljano <Emiljanokurbiba@gmail.com>
2020-07-12 16:40:20 +0200

    update test script

--- jenkins/scripts/testing.sh
+++ jenkins/scripts/testing.sh
@@ -1,7 +1,6 @@
 ssh jenkins@projectmanager << EOF
 cd DevOps-Practical
 git pull
-sudo env TEST_DB_URI=${TEST_DB_URI}
 echo $TEST_DB_URI
-python3 -m pytest --cov service-one/application --cov-report term-missing
+sudo env TEST_DB_URI=${TEST_DB_URI} python3 -m pytest --cov service-one/application --cov-report term-missing
 EOF
