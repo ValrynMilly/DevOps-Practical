a84088e857e5902137ce416314ff050f9af62b2d
Emiljano <Emiljanokurbiba@gmail.com>
2020-07-12 16:25:00 +0200

    testing scripts

--- jenkins/scripts/testing.sh
+++ jenkins/scripts/testing.sh
@@ -1,8 +1,8 @@
-ssh projectmanager << EOF
+ssh jenkins@projectmanager << EOF
 rm -r DevOps-Practical
 git clone https://github.com/ValrynMilly/DevOps-Practical.git -b Jenkins
 
 cd DevOps-Practical
 
-python3 -m pytest --cov application --cov-report term-missing
+python3 -m pytest --cov service-one/application --cov-report term-missing
 EOF
