7dc9518e3d7dc9ce0f733ff04e982f5f93928d5d
Emiljano <Emiljanokurbiba@gmail.com>
2020-07-12 19:44:16 +0200

    adding testing for service three

--- jenkins/scripts/testing.sh
+++ jenkins/scripts/testing.sh
@@ -5,4 +5,5 @@ echo $TEST_DB_URI
 cd service-one
 sudo env TEST_DB_URI=${TEST_DB_URI} python3 -m pytest --cov service-one
 sudo env TEST_DB_URI=${TEST_DB_URI} python3 -m pytest --cov service-two/tests
+sudo env TEST_DB_URI=${TEST_DB_URI} python3 -m pytest --cov service-three/tests
 EOF
