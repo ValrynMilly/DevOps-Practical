e51965758702f6a815463424e210e620c46dd400
Emiljano <Emiljanokurbiba@gmail.com>
2020-07-12 03:23:13 +0200

    CSRF requred now

--- Jenkinsfile
+++ Jenkinsfile
@@ -1,10 +1,20 @@
 pipeline{
   agent any
   stages{
+    stage('Ansible Install & Verify (Also installs docker)'){
+      steps{
+        sh "bash jenkins/scripts/ansibleinstallverify.sh"
+      }
+    } 
+    stage('Stack Set-up'){
+      steps{
+        sh "bash jenkins/scripts/stacksonstacks.sh"   
+      }
+    }
     stage('Testing'){
       steps{
         sh "bash jenkins/scripts/testing.sh"
       }
-    }  
+    } 
   }
 }
--- service-one/tests/test_unit.py
+++ service-one/tests/test_unit.py
@@ -4,16 +4,16 @@ import requests_mock
 from flask_testing import TestCase
 from application import routes
 from application import app
+from application.routes import namegenform_post
 
 class TestBase(TestCase):
     def create_app(self):
         return app
 
 class TestGenerate(TestBase):
-    def test_homepage(self):
-        with requests_mock.mock() as mock_get:
-            # getting successful response by 200 code
-            mock_get.get("http://service-two:5001", text="irosAafkeilir")
-            response = self.client.post(url_for('namegenform_post'))
-            
-            self.assertIn(b'irosAafkeilir', response)
+    def test_ifpagefound(self):
+        with patch('requests.get') as mock_request:
+            servicetwo = 'http://service-two:5001'
+            mock_request.return_value.result = "Fake content"
+            response = namegenform_post()
+            self.assertNotNone(response.result)
