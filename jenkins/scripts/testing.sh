c59fc0f29e5f2c1444200bd108a1f7f30bdc6b95
Emiljano <Emiljanokurbiba@gmail.com>
2020-07-12 19:23:24 +0200

    updated test script

--- jenkins/scripts/testing.sh
+++ jenkins/scripts/testing.sh
@@ -8,6 +8,6 @@ cd ..
 cd service-two
 sudo env TEST_DB_URI=${TEST_DB_URI} python3 -m pytest --cov tests --cov-report term-missing
 cd
-cd service-two
+cd service-three
 sudo env TEST_DB_URI=${TEST_DB_URI} python3 -m pytest --cov tests --cov-report term-missing
 EOF
