9ca3eb59a0a490196232ec1be1243054e2432730
Emiljano <Emiljanokurbiba@gmail.com>
2020-07-12 16:31:47 +0200

    trying manual uri

--- jenkins/scripts/testing.sh
+++ jenkins/scripts/testing.sh
@@ -1,6 +1,7 @@
 ssh jenkins@projectmanager << EOF
 cd DevOps-Practical
 git pull
+export TEST_DB_URI='mysql+pymysql://root:Le0535892@35.242.172.253/testfantasygendb'
 echo $TEST_DB_URI
 python3 -m pytest --cov service-one/application --cov-report term-missing
 EOF
