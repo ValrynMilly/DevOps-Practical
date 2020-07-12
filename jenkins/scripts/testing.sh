b418d030c409d9e3039370f5e824a9c2697a6eec
Emiljano <Emiljanokurbiba@gmail.com>
2020-07-12 16:14:34 +0200

    update testing script

--- jenkins/scripts/testing.sh
+++ jenkins/scripts/testing.sh
@@ -4,5 +4,5 @@ sudo apt install python3-pip python3-venv -y
 pip3 install flask flask-testing pytest pytest-cov requests
 pip3 install requests_mock
 env SECRET_KEY=${SECRET_KEY}
-env TEST_DB_URI=${TEST_DB_URI}
+env SQLALCHEMY_DATABASE_URI=${TEST_DB_URI}
 python3 -m pytest ./service-one/tests/test_unit.py
