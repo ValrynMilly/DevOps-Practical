c564d1558029e5ecd2de4c7070573ff2ec776c45
Emiljano <Emiljanokurbiba@gmail.com>
2020-07-12 19:56:03 +0200

    added tests for service four

--- jenkins/scripts/testing.sh
+++ jenkins/scripts/testing.sh
@@ -3,7 +3,8 @@ cd DevOps-Practical
 git pull
 echo $TEST_DB_URI
 cd service-one
-sudo env TEST_DB_URI=${TEST_DB_URI} python3 -m pytest --cov service-one --cov-report term-missing
-sudo env TEST_DB_URI=${TEST_DB_URI} python3 -m pytest --cov service-two/tests --cov-report term-missing
-sudo env TEST_DB_URI=${TEST_DB_URI} python3 -m pytest --cov service-three/tests --cov-report term-missing
+sudo env TEST_DB_URI=${TEST_DB_URI} python3 -m pytest service-one --cov-report term-missing
+sudo env TEST_DB_URI=${TEST_DB_URI} python3 -m pytest service-two/tests --cov-report term-missing
+sudo env TEST_DB_URI=${TEST_DB_URI} python3 -m pytest service-three/tests --cov-report term-missing
+sudo env TEST_DB_URI=${TEST_DB_URI} python3 -m pytest service-four/tests --cov-report term-missing
 EOF
