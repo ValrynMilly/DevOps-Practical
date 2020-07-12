d211bba563b4e9523400bfd6ee58c240befe5379
Emiljano <Emiljanokurbiba@gmail.com>
2020-07-12 16:26:42 +0200

    echo added for pipeline debugging

--- jenkins/scripts/testing.sh
+++ jenkins/scripts/testing.sh
@@ -3,6 +3,6 @@ rm -r DevOps-Practical
 git clone https://github.com/ValrynMilly/DevOps-Practical.git -b Jenkins
 
 cd DevOps-Practical
-
+echo $TEST_DB_URI
 python3 -m pytest --cov service-one/application --cov-report term-missing
 EOF
