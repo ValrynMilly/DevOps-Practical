80181cb533c07a1c60525646975eaf80ec6d128f
Emiljano <Emiljanokurbiba@gmail.com>
2020-07-12 20:03:36 +0200

    back to just 3

--- jenkins/scripts/testing.sh
+++ jenkins/scripts/testing.sh
@@ -6,5 +6,5 @@ cd service-one
 sudo env TEST_DB_URI=${TEST_DB_URI} python3 -m pytest service-one --cov-report term-missing
 sudo env TEST_DB_URI=${TEST_DB_URI} python3 -m pytest service-two/tests --cov-report term-missing
 sudo env TEST_DB_URI=${TEST_DB_URI} python3 -m pytest service-three/tests --cov-report term-missing
-pytest ./service-four/tests/test_story.py --cov-report term-missing
+
 EOF
