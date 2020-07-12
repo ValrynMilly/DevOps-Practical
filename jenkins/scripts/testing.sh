c51bbb847381b5db6d24ab4f0d4697a109f8749a
Emiljano <Emiljanokurbiba@gmail.com>
2020-07-12 19:59:41 +0200

    update testing script

--- jenkins/scripts/testing.sh
+++ jenkins/scripts/testing.sh
@@ -1,4 +1,6 @@
 ssh jenkins@projectmanager << EOF
+sudo rm -rf DevOps-Practical
+git clone https://github.com/ValrynMilly/DevOps-Practical.git -b Jenkins
 cd DevOps-Practical
 git pull
 echo $TEST_DB_URI
