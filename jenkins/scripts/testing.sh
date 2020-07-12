cad83f5a52edf2c4bfcfe7dc0910eaef5736418e
Emiljano <Emiljanokurbiba@gmail.com>
2020-07-12 16:30:17 +0200

    updated script

--- jenkins/scripts/testing.sh
+++ jenkins/scripts/testing.sh
@@ -1,8 +1,6 @@
 ssh jenkins@projectmanager << EOF
-rm -r DevOps-Practical
-git clone https://github.com/ValrynMilly/DevOps-Practical.git -b Jenkins
-
 cd DevOps-Practical
+git pull
 echo $TEST_DB_URI
 python3 -m pytest --cov service-one/application --cov-report term-missing
 EOF
