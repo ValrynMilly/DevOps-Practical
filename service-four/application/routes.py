f44b8fb47cc182bd1b6813c5b59f1709eb285f2a
Emiljano <Emiljanokurbiba@gmail.com>
2020-07-13 14:14:07 +0200

    live change

--- service-four/application/routes.py
+++ service-four/application/routes.py
@@ -14,10 +14,11 @@ def titlegen_post():
     name = requests.get(servicetwo)
     #title is defined then makes a request to service thee to give it a value
     title = requests.get(servicethree)
+    intro = "Once upon a time"
     where = ['In a bar','On a field', 'Through the clouds', 'Under a mountain', 'Underwater', 'In a town']
     action = ['killed', 'annihilated', 'destroyed', 'Poisoned', 'Petrified', 'Saved', 'Rescued', 'Healed', 'Silenced']
     instrument = ['a pinkie', 'a pencil', 'a hammer', 'a bow', 'lightning', 'fire', 'a hunt'] 
     #short story is a combination of lists randomly picked but within the story name and title are reference so the result makes sense
-    shortstory = (random.choice(where) + " " + name.text + " " + title.text +" Once " + random.choice(action)+ " " + str(random.randint(1, 3000)) + " MEN WITH " + random.choice(instrument)) 
+    shortstory = (intro + random.choice(where) + " " + name.text + " " + title.text +" Once " + random.choice(action)+ " " + str(random.randint(1, 3000)) + " MEN WITH " + random.choice(instrument)) 
     #then returns a response with the short story
     return Response(shortstory.upper(), mimetype='text/plain')
