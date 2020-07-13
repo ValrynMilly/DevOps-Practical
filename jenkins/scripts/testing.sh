e49076b41569ba8cc7affcdb128a8edbe7ae23cb
Emiljano <Emiljanokurbiba@gmail.com>
2020-07-13 02:17:51 +0200

    tring testing script for coverage

--- jenkins/scripts/testing.sh
+++ jenkins/scripts/testing.sh
@@ -3,8 +3,8 @@ cd DevOps-Practical
 git pull
 echo $TEST_DB_URI
 cd service-one
-sudo env TEST_DB_URI=${TEST_DB_URI} python3 -m pytest --cov service-one
-sudo env TEST_DB_URI=${TEST_DB_URI} python3 -m pytest --cov service-two/tests
-sudo env TEST_DB_URI=${TEST_DB_URI} python3 -m pytest --cov service-three/tests
-sudo env TEST_DB_URI=${TEST_DB_URI} python3 -m pytest --cov service-four/tests
+sudo env TEST_DB_URI=${TEST_DB_URI} python3 -m pytest --cov service-one --cov-report term-missing
+sudo env TEST_DB_URI=${TEST_DB_URI} python3 -m pytest --cov service-two/tests --cov-report term-missing
+sudo env TEST_DB_URI=${TEST_DB_URI} python3 -m pytest --cov service-three/tests --cov-report term-missing
+sudo env TEST_DB_URI=${TEST_DB_URI} python3 -m pytest --cov service-four/tests --cov-report term-missing
 EOF
